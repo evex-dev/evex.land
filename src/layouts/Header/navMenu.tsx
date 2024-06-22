f87f2ad357e2d5f5146e677b113674037f4060b7
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-06-23 00:29:45 +0900

    wip

--- src/layouts/Header/navMenu.tsx
+++ src/layouts/Header/navMenu.tsx
@@ -6,25 +6,45 @@ import {
 	NavigationMenuList,
 	NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
-import { Suspense } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export function NavMenu() {
 	return (
 		<NavigationMenu>
 			<NavigationMenuList>
 				<NavigationMenuItem>
-					<NavigationMenuTrigger className="bg-black/5">
+					<NavigationMenuTrigger className="bg-transparent">
                         Blog
                     </NavigationMenuTrigger>
 					<NavigationMenuContent>
-						<ul className="grid gap-3 p-6">
-                            <Suspense fallback={<div>Loading...</div>}>
-                                aa
-                            </Suspense>
+						<ul className="grid gap-3 p-2">
+							<BlogList />
 						</ul>
 					</NavigationMenuContent>
 				</NavigationMenuItem>
 			</NavigationMenuList>
 		</NavigationMenu>
 	);
+}
+
+
+function BlogList() {
+	const blogFetcher = () => fetch("/rss.xml").then((res) => res.text()).then((xml) => new window.DOMParser().parseFromString(xml, "text/xml"));
+	const blogFetcherCallback = useCallback(() => blogFetcher(), [])
+	const [posts, setPosts] = useState<Record<"title" | "link", string>[]>([]);
+
+	useEffect(() => {
+		blogFetcherCallback().then((data) => {
+			setPosts(Array.from(data.querySelectorAll("item")).map((item) => ({
+				title: item.querySelector("title")!.textContent ?? "",
+				link: new window.URL(item.querySelector("link")!.textContent ?? window.location.href).pathname,
+			})));
+		});
+	}, [blogFetcherCallback]);
+
+	return <div className="grid grid-cols-2 gap-3 w-[250px]">
+		{posts.slice(0, 6).map((post) => <a href={post.link} className="text-sm hover:underline" key={post.title}>
+			<Button variant="ghost" className="text-xs hover:underline w-[120px]">{post.title.length > 12 ? `${post.title.slice(0, 10)}...` : post.title}</Button>
+		</a>)}
+	</div>;
 }
