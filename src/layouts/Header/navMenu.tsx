e002f7dc00bcb94fdcf4645b25b1b3723952d2f1
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-06-23 11:53:26 +0900

    wip

--- src/layouts/Header/navMenu.tsx
+++ src/layouts/Header/navMenu.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import {
 	NavigationMenu,
 	NavigationMenuContent,
@@ -6,6 +5,7 @@ import {
 	NavigationMenuList,
 	NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
+import { Separator } from "@/components/ui/separator";
 import { useState, useEffect, useCallback } from "react";
 
 export function NavMenu() {
@@ -13,13 +13,12 @@ export function NavMenu() {
 		<NavigationMenu>
 			<NavigationMenuList>
 				<NavigationMenuItem>
-					<NavigationMenuTrigger className="bg-transparent">
-                        Blog
-                    </NavigationMenuTrigger>
+					<NavigationMenuTrigger className="bg-transparent">Blog</NavigationMenuTrigger>
 					<NavigationMenuContent>
-						<ul className="grid gap-3 p-2">
+						<div className="grid gap-3 p-2">
+							<div className="text-md font-medium text-center text-gray-400">最新の更新</div>
 							<BlogList />
-						</ul>
+						</div>
 					</NavigationMenuContent>
 				</NavigationMenuItem>
 			</NavigationMenuList>
@@ -27,24 +26,42 @@ export function NavMenu() {
 	);
 }
 
-
 function BlogList() {
-	const blogFetcher = () => fetch("/rss.xml").then((res) => res.text()).then((xml) => new window.DOMParser().parseFromString(xml, "text/xml"));
-	const blogFetcherCallback = useCallback(() => blogFetcher(), [])
-	const [posts, setPosts] = useState<Record<"title" | "link", string>[]>([]);
+	const blogFetcher = () =>
+		fetch("/rss.xml")
+			.then(res => res.text())
+			.then(xml => new window.DOMParser().parseFromString(xml, "text/xml"));
+	const blogFetcherCallback = useCallback(() => blogFetcher(), []);
+	const [posts, setPosts] = useState<Record<"title" | "link" | "description", string>[]>([]);
 
 	useEffect(() => {
-		blogFetcherCallback().then((data) => {
-			setPosts(Array.from(data.querySelectorAll("item")).map((item) => ({
-				title: item.querySelector("title")!.textContent ?? "",
-				link: new window.URL(item.querySelector("link")!.textContent ?? window.location.href).pathname,
-			})));
+		blogFetcherCallback().then(data => {
+			setPosts(
+				Array.from(data.querySelectorAll("item")).map(item => ({
+					title: item.querySelector("title")!.textContent ?? "",
+					link: new window.URL(item.querySelector("link")!.textContent ?? window.location.href).pathname,
+					description: item.querySelector("description")!.textContent ?? "",
+				})),
+			);
 		});
 	}, [blogFetcherCallback]);
 
-	return <div className="grid grid-cols-2 gap-3 w-[250px]">
-		{posts.slice(0, 6).map((post) => <a href={post.link} className="text-sm hover:underline" key={post.title}>
-			<Button variant="ghost" className="text-xs hover:underline w-[120px]">{post.title.length > 12 ? `${post.title.slice(0, 10)}...` : post.title}</Button>
-		</a>)}
-	</div>;
+	return (
+		<div className="grid gap-3 w-[160px] md:w-[200px]">
+			{posts.slice(0, 6).map(post => (
+				<>
+					<Separator />
+					<a href={post.link} className="text-sm" key={post.title}>
+						<div className="w-[160px] md:w-[200px] h-[50px] flex flex-col">
+							<p className="truncate font-medium">
+								<span className="hidden md:inline">{post.title.length > 20 ? `${post.title.slice(0, 17)}...` : post.title}</span>
+								<span className="inline md:hidden">{post.title.length > 15 ? `${post.title.slice(0, 14)}...` : post.title}</span>
+							</p>
+							<p className="text-gray-400 truncate text-xs">{post.description}</p>
+						</div>
+					</a>
+				</>
+			))}
+		</div>
+	);
 }
